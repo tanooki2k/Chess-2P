ebcb651c3052b3e385bccb47fd61c0bb434b88cc
Javier Rojas Ramirez <javierito.rojas.ramirez@gmail.com>
2025-02-05 21:49:42 +0000

    Common pieces class added && Directories and Files renamed

---
+++ Chess_pieces/Common.py
@@ -0,0 +1,26 @@
+import json
+
+
+class Common:
+    alive = 0
+
+    def __init__(self, kind, team):
+        self.type = kind
+        self.team = team
+
+        try:
+            self.display = pieces[f"{self.team}"][f"{self.type}"]
+        except KeyError:
+            raise KeyError(
+                f"Invalid piece configuration: Type '{self.type}' or Team '{self.team}' is not defined. Please check the pieces definition.")
+
+    def move(self):
+        pass
+
+
+path = '/home/javier/PycharmProjects/Chess2P/pieces.json'
+with open(path) as file:
+    pieces = json.load(file)[0]
+
+if __name__ == "__main__":
+    print(pieces)
--- game.py
+++ game.py
@@ -1,21 +1,34 @@
+from Chess_pieces.Common import Common
+
+
 class Game:
     blocks = {"white": "O", "black": "#"}  # The symbol that represent the chess' blocks.
 
     def __init__(self, player1="Player 1", player2="Player 2"):
         self.board = [
-            [self.blocks["white"] if (i * 7 + j) % 2 else self.blocks["black"] for j in range(8)]
-            for i in range(8)
+            [Common("Pawn", "Black"), Common("King", "Black"), 0, 0, 0, 0, 0, 0],
+            [0] * 8,
+            [0] * 8,
+            [0] * 8,
+            [0] * 8,
+            [0] * 8,
+            [0] * 8,
+            [0, 0, 0, 0, 0, 0, 0, Common("Pawn", "White")]
+
         ]
         self.player1 = player1
         self.player2 = player2
 
     def __repr__(self):
-        col = '  '.join([chr(i + 65) for i in range(len(self.board))])
-        game_board = f"\t{col}\n"  # Create the guide A-H columns coordinates.
+        cols = '  '.join([chr(i + 65) for i in range(len(self.board))])
+        game_board = f"\t{cols}\n"  # Create the guide A-H columns coordinates.
         for row_idx in range(len(self.board)):  # Generate all the Chess bord with its respective blocks and pieces.
             game_board += f"{8 - row_idx}\t"
-            for col in self.board[row_idx]:
-                game_board += col + "  "
+            for col_idx in range(len(self.board[row_idx])):
+                if self.board[row_idx][col_idx]:
+                    game_board += f"{self.board[row_idx][col_idx].display}  "
+                else:
+                    game_board += f"{self.blocks['black'] if (row_idx * 7 + col_idx) % 2 else self.blocks['white']}  "
             game_board += "\n"
 
         return game_board  # Return the chess board
